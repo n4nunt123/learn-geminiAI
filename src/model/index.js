e2673187b0bf940b491059d644be8cbebae35a2d
unknown <ihellostereo@gmail.com>
2025-06-24 14:25:56 +0700

    v1.2.4/leveling model

---
+++ src/model/index.js
@@ -0,0 +1,7 @@
+const { GoogleGenAI } = require('@google/genai');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GENAI_API_KEY });
+
+module.exports = ai;
