2109936f839c67ca6410b8678f8e55f76ab29401
unknown <ihellostereo@gmail.com>
2025-06-25 21:32:21 +0700

    v2.0.0/unit test isMissingValue conditional utils

--- src/utils/conditionalUtil.test.js
+++ src/utils/conditionalUtil.test.js
@@ -2,6 +2,7 @@ const { NOP, isMissingValue } = require('./conditionalUtil');
 
 describe('#conditionalUtil', () => {
   const resultNOP = undefined;
+  const val = 'Generate something';
   const valName = 'command';
   const code = 'MISSING_VALUE';
   const errorMessage = `Variable "${valName}" is missing (null or undefined)`;
@@ -16,6 +17,12 @@ describe('#conditionalUtil', () => {
 
   it('should return nothing || undefined when NOP is invoked', async () => {
     const result = NOP();
+
+    expect(result).toEqual(resultNOP);
+  });
+
+  it('should return nothing when isMissingValue is invoked and nothing is missing', () => {
+    const result = isMissingValue(val, valName);
 
     expect(result).toEqual(resultNOP);
   });
